bd0ec59a49152f0a92d21e8e16f91233e1c2e8a4
koyanagi <koyakeiaaaaa@gmail.com>
2023-09-04 00:30:19 +0900

    Initial Commit

---
+++ MotionSender/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MotionSender
+//
+//  Created by koyanagi on 2023/09/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MotionSender/MotionSenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotionSenderApp.swift
+//  MotionSender
+//
+//  Created by koyanagi on 2023/09/04.
+//
+
+import SwiftUI
+
+@main
+struct MotionSenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
